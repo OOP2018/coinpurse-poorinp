55abf60662bb9e65018bfacc459fb8a865c0d7ed
Poorin Pichayamongkol <poorinsmoothy@hotmail.com>
2018-04-20 14:28:19 +0700

    lab 11 Generics

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -1,6 +1,7 @@
 package coinpurse;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Collection;
@@ -15,15 +16,15 @@ public class MoneyUtil {
 	 * Sorting the Collections about value of coins.
 	 * @param coins is the coin in List to sort.
 	 */
-	public static void sortCoins(List<Coin> coins){
-		java.util.Collections.sort(coins);
+	public static void sortCoins(List<? extends Valuable> money){
+		java.util.Collections.sort(money);
 	}
 	
 	/**
 	 * print the solution of coins
 	 * @param coins is the coin in List to print
 	 */
-	public static void printCoins(List<Valuable> money) {
+	public static void printCoins(List<? extends Valuable> money) {
 		for (Valuable moneyValue : money) {
 			System.out.println(moneyValue);
 		}
@@ -35,9 +36,9 @@ public class MoneyUtil {
 	 * @param currency is currency to sort by
 	 * @return the new List that have only the same currency coins
 	 */
-	public static List<Valuable> filterByCurrency(List<Valuable> money, String currency) {
-		List<Valuable> temp = new ArrayList<>();
-		for (Valuable moneyValue: money) {
+	public static <E extends Valuable> List<E> filterByCurrency(List<E> money, String currency) {
+		List<E> temp = new ArrayList<>();
+		for (E moneyValue: money) {
 			if (moneyValue.getCurrency().equals(currency)) {
 				temp.add(moneyValue);
 			}
@@ -45,8 +46,25 @@ public class MoneyUtil {
 		return temp;
 	}
 	
+	/**
+	 * Return the larger of a and b, according to the natural
+	 * ordering (defined by compareTo).
+	 * @param args one or more Comparable objects to compare.
+	 * @throws IllegalArgumentException if no arguments given.
+	 */
+	public static <E extends Comparable<? super E>> E max(E ... args) {
+		E max = null;
+		for (int i = 0; i < args.length - 1; i++) {
+			if (max == null) max = args[i];
+			if (args[i].compareTo(max) > 0) {
+				max = args[i];
+			}
+		}
+		return max;
+	}
+	
 	//test MoneyUtil
-//	public static void main(String[] args) {
+	public static void main(String[] args) {
 //		List<Valuable> money = new ArrayList<Valuable>();
 //		money.add( new Coin(10.0, "Baht") );
 //		money.add( new Coin(3.5, "Baht") );
@@ -59,5 +77,25 @@ public class MoneyUtil {
 //		System.out.println("-------------------");
 //		money = filterByCurrency(money, "Baht");
 //		printCoins(money);
-//	}
+		
+//		String max = MoneyUtil.max( "dog", "zebra", "cat");
+//		System.out.println(max);
+		
+		Money m1 = new BankNote(10, "Baht", 0);
+		Money m2 = new BankNote(100, "Baht", 0);
+		Money m3 = new Coin(20, "Baht");
+		Money max = MoneyUtil.max(m1, m2, m3);
+		System.out.println(max);
+		
+		List<BankNote> list = new ArrayList<BankNote>();
+		list.add(new BankNote(10.0, "USD", 0));
+		list.add(new BankNote(500.0, "Baht", 0));
+		MoneyUtil.sortCoins(list);
+		System.out.println(list);
+		
+		List<Coin> coins = Arrays.asList( new Coin(5, "Baht"), new Coin(0.1, "Ringgit"), new Coin(5, "Cent") );
+		List<Coin> result = MoneyUtil.filterByCurrency(coins, "Baht");
+		System.out.println(result);
+		
+	}
 }
